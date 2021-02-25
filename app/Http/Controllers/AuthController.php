5e412e2788be5b00d0f59bf10756634878282fb0
martin_dev <gmsuruguay@gmail.com>
2021-02-25 18:13:27 -0300

    se creo metodo signup

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
@@ -29,6 +30,30 @@ class AuthController extends Controller
 
         $user = $request->user();
         $token = $user->createToken('hJnvyHv6kGm8r1H5RvbRvikvNzGSrJysjPkEBq0y')->accessToken;
+        $response = ['token' => $token, 'user'=> $user];
+
+        return response($response, 200);
+      
+    }
+
+    public function signup(Request $request){
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users|max:255',
+            'password' => 'required|min:6',
+            'confirm_password' => 'required|same:password',
+        ]);
+
+        if ($validator->fails())
+        {
+            return response(['errors'=>$validator->errors()->all()], 422);
+        }
+
+        $input = $request->all();
+        $input['password'] = bcrypt($request->get('password'));
+        $user = User::create($input);
+        $token = $user->createToken('MyApp')->accessToken;
         $response = ['token' => $token, 'user'=> $user];
 
         return response($response, 200);
--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,7 @@ Route::group([
     'prefix'=>'auth'
     ], function(){
         Route::post('login', 'AuthController@login');  
+        Route::post('signup', 'AuthController@signup');  
 
 });
 
